a273a0f83ff30f28b9230eba2f6e343766694ca3
Jeff Candell <jcandell@wustl.edu>
2021-03-11 14:10:19 -0600

    Table shell, collection page

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 document.getElementById('beginButton').addEventListener('click', switchDataCollect)
 let constDir = 'x'
-let constVal = 0
-let otherVals = []
+let xCoords = 0
+let yCoords = 0
+const directions = ['N', 'E', 'S', 'W']
 function switchDataCollect() {
     constDir = document.querySelector('input[name="dir"]:checked').value
-    constVal = document.getElementById('constValInput').value
-    otherVals = document
-        .getElementById('rangeValues')
-        .value.split(',')
-        .map((num) => Number(num))
-    console.log(constDir, constVal, otherVals)
+    if (constDir === 'x') {
+        xCoords = Number(document.getElementById('constValInput').value)
+        yCoords = document
+            .getElementById('rangeValues')
+            .value.split(',')
+            .map((num) => Number(num))
+    } else {
+        yCoords = Number(document.getElementById('constValInput').value)
+        xCoords = document
+            .getElementById('rangeValues')
+            .value.split(',')
+            .map((num) => Number(num))
+    }
+    document.getElementById('entry').style.display = 'none'
+    document.getElementById('collection').style.display = 'block'
+    document.getElementById('currX').innerText =
+        typeof xCoords === 'number' ? xCoords : xCoords[0]
+    document.getElementById('currY').innerText =
+        typeof yCoords === 'number' ? yCoords : yCoords[0]
+    document.getElementById('currDir').innerText = directions[0]
 }
